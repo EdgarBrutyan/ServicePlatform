cd5f735984a3a4fc6647b477797cf578ab6d4457
EdgarBrutyan <115035402+EdgarBrutyan@users.noreply.github.com>
2023-10-01 19:17:38 +0400

    Add files via upload

---
+++ CommandService/CommandService/CommandService/Controllers/PlatformsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.Eventing.Reader;
+
+namespace CommandService.Controllers
+{
+    [Route("api/c/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        public PlatformsController() { }
+
+        [HttpPost]
+        public ActionResult Connection()
+        {
+            Console.WriteLine("The platform is accepted");
+            return Ok("The command service connects to PlatformService");
+        }
+    }
+}
